bb0b0406eff2854ee60e919ec66430fc13042bff
kkentlisa <e.kkenig@gmail.com>
2026-02-20 20:05:45 +0700

    добавила функции evaluateExpression и EvaluateCondition, вычисляющие арифметические блоки, блоки со сравнением и логическими операциями

--- scripts/interpreter.js
+++ scripts/interpreter.js
@@ -8,7 +8,7 @@ function GetVariable(name) {
     return variables[name];
 }
 
-function evaluateBlock(block) {
+function evaluateExpression(block) {
     if (block.type === "input") {
         const value = block.data.value;
         if (isNaN(value)) {
@@ -18,47 +18,102 @@ function evaluateBlock(block) {
             return Number(value);
     }
 
-    if (block.type === "add") {
-        const leftValue = evaluateBlock(block.data.left);
-        const rightValue = evaluateBlock(block.data.right);
+    else if (block.type === "add") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
         return leftValue + rightValue;
     }
 
-    if (block.type === "subtract") {
-        const leftValue = evaluateBlock(block.data.left);
-        const rightValue = evaluateBlock(block.data.right);
+    else if (block.type === "subtract") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
         return leftValue - rightValue;
     }
 
-    if (block.type === "multiply") {
-        const leftValue = evaluateBlock(block.data.left);
-        const rightValue = evaluateBlock(block.data.right);
+    else if (block.type === "multiply") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
         return leftValue * rightValue;
     }
 
-    if (block.type === "div") {
-        const leftValue = evaluateBlock(block.data.left);
-        const rightValue = evaluateBlock(block.data.right);
+    else if (block.type === "div") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
 
         if (rightValue === 0) {
             // пока просто вывод в консоль
             console.log("Деление на 0!")
-            return
+            return 0;
         }
 
         return Math.floor(leftValue / rightValue);
     }
 
-    if (block.type === "mod") {
-        const leftValue = evaluateBlock(block.data.left);
-        const rightValue = evaluateBlock(block.data.right);
+    else if (block.type === "mod") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
 
         if (rightValue === 0) {
             // пока просто вывод в консоль
             console.log("Деление на 0!")
-            return
+            return 0;
         }
 
         return leftValue % rightValue;
+    }
+}
+
+function EvaluateCondition(block) {
+    if (block.type === "gt") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
+        return leftValue > rightValue;
+    }
+
+    else if (block.type === "lt") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
+        return leftValue < rightValue;
+    }
+
+    else if (block.type === "eq") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
+        return leftValue === rightValue;
+    }
+
+    else if (block.type === "neq") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
+        return leftValue !== rightValue;
+    }
+
+    else if (block.type === "gte") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
+        return leftValue >= rightValue;
+    }
+
+    else if (block.type === "lte") {
+        const leftValue = evaluateExpression(block.data.left);
+        const rightValue = evaluateExpression(block.data.right);
+        return leftValue <= rightValue;
+    }
+
+    else if (block.type === "and") {
+        const leftValue = EvaluateCondition(block.data.left);
+        const rightValue = EvaluateCondition(block.data.right);
+        return leftValue && rightValue;
+    }
+
+    else if (block.type === "or") {
+        const leftValue = EvaluateCondition(block.data.left);
+        const rightValue = EvaluateCondition(block.data.right);
+        return leftValue || rightValue;
+    }
+
+    else if (block.type === "not") {
+        const operandValue = EvaluateCondition(block.data.operand);
+        return !operandValue;
     }
 }
